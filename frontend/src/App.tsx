2b31c0eae925da65647ec65155580e6876cd6b7f
이태원 <ltw971@naver.com>
2021-12-24 17:07:41 +0900

    git test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ const App = function () {
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
+					Test
 				</a>
 			</header>
 		</div>
