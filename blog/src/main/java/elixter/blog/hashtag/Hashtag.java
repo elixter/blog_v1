4e3476991c81e837ff0789116acdac455ca250fc
elixter <ltw971@naver.com>
2022-01-05 17:17:14 +0900

    feat: hashtag model

---
+++ blog/src/main/java/elixter/blog/hashtag/Hashtag.java
@@ -0,0 +1,27 @@
+package elixter.blog.hashtag;
+
+public class Hashtag {
+    private Long id;
+    private String value;
+
+    public Hashtag(Long id, String value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ blog/src/sql/hashtags.sql
@@ -0,0 +1,6 @@
+create table hashtags
+(
+    id    bigint auto_increment
+        primary key,
+    value varchar(50) not null
+);
