de8d5b38b69ec8c6ea0c5a7801ab13e8e09fd3c8
elixter <ltw971@naver.com>
2022-01-20 18:16:52 +0900

    feat: define equals method for post for testing

--- blog/ddl/posts.sql
+++ blog/ddl/posts.sql
@@ -2,9 +2,11 @@ create table posts
 (
     id        bigint auto_increment
         primary key,
-    title     varchar(50) not null,
-    content   text        not null,
-    thumbnail text        null,
-    category  varchar(20) not null,
-    status    varchar(10) not null
+    title     varchar(50)                         not null,
+    content   text                                not null,
+    thumbnail text                                null,
+    category  varchar(20)                         not null,
+    status    varchar(10)                         not null,
+    create_at timestamp default CURRENT_TIMESTAMP not null,
+    update_at timestamp default CURRENT_TIMESTAMP not null
 );
