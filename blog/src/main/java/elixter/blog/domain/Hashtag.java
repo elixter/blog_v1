1efcea21a024c68dff939bd0d9e012339808052e
이태원 <ltw971@naver.com>
2022-01-26 15:38:45 +0900

    feat: JdbcTemplateHashtagRepository

--- blog/src/main/java/elixter/blog/domain/Hashtag.java
+++ blog/src/main/java/elixter/blog/domain/Hashtag.java
@@ -15,6 +15,7 @@ public class Hashtag {
     private Long postId;
 
     // TODO: hashtag DDL value 이름 tag로 바꾸기
+    // TODO: hashtag 테이블에 status 컬럼 추가
     public Hashtag() {
     }
 
---
+++ blog/src/main/java/elixter/blog/repository/hashtag/JdbcTemplateHashtagRepository.java
@@ -0,0 +1,88 @@
+package elixter.blog.repository.hashtag;
+
+import elixter.blog.Constants;
+import elixter.blog.domain.Hashtag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class JdbcTemplateHashtagRepository implements HashtagRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcTemplateHashtagRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Hashtag save(Hashtag hashtag) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("hashtags").usingGeneratedKeyColumns("id");
+
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("tag", hashtag.getTag());
+        parameters.put("post_id", hashtag.getPostId());
+        parameters.put("status", Constants.recordStatusExist);
+
+        Number key = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        hashtag.setId(key.longValue());
+
+        return hashtag;
+    }
+
+    @Override
+    public Optional<Hashtag> findById(Long id) {
+        List<Hashtag> result = jdbcTemplate.query("select * from hashtags where id = ? and status = ?", hashtagRowMapper(), id, Constants.recordStatusExist);
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Hashtag> findByTag(String tag) {
+        return jdbcTemplate.query("select * from hashtags where tag = ? and status = ?", hashtagRowMapper(), tag, Constants.recordStatusExist);
+    }
+
+    @Override
+    public List<Hashtag> findAll() {
+        return jdbcTemplate.query("select * from hashtags where status = ?", hashtagRowMapper(), Constants.recordStatusExist);
+    }
+
+    @Override
+    public List<Hashtag> findByPostId(Long postId) {
+        return jdbcTemplate.query("select * from hashtags where post_id = ? and status = ?", hashtagRowMapper(), postId, Constants.recordStatusExist);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        jdbcTemplate.update("update hashtags set status = ? where id = ?", hashtagRowMapper(), Constants.recordStatusDeleted, id);
+    }
+
+    @Override
+    public void deleteByTag(String tag) {
+        jdbcTemplate.update("update hashtags set status = ? where tag = ?", hashtagRowMapper(), Constants.recordStatusDeleted, tag);
+    }
+
+    private RowMapper<Hashtag> hashtagRowMapper() {
+        return new RowMapper<Hashtag>() {
+            @Override
+            public Hashtag mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Hashtag hashtag = new Hashtag();
+                hashtag.setId(rs.getLong("id"));
+                hashtag.setTag(rs.getString("tag"));
+                hashtag.setPostId(rs.getLong("post_id"));
+
+                return hashtag;
+            }
+        };
+    }
+}
