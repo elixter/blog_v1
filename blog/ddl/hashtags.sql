65732377345b3894581a6046407309e2d4952674
elixter <ltw971@naver.com>
2022-01-26 17:45:41 +0900

    feat: modified hashtags table DDL

--- blog/ddl/hashtags.sql
+++ blog/ddl/hashtags.sql
@@ -2,8 +2,9 @@ create table hashtags
 (
     id      bigint auto_increment
         primary key,
-    value   varchar(50) not null,
+    tag     varchar(50) not null,
     post_id bigint      not null,
+    status  varchar(10) not null,
     constraint hashtags_posts_id_fk
         foreign key (post_id) references posts (id)
             on update cascade on delete cascade
