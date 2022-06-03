95e36adefe57cc713ee569bc67b991828c1196e0
elixter <ltw971@naver.com>
2022-06-03 14:25:14 +0900

    feat: searchTag test

--- blog/src/test/java/elixter/blog/repository/hashtag/JpaHashtagRepositoryTest.java
+++ blog/src/test/java/elixter/blog/repository/hashtag/JpaHashtagRepositoryTest.java
@@ -2,6 +2,7 @@ package elixter.blog.repository.hashtag;
 
 import elixter.blog.constants.RecordStatus;
 import elixter.blog.domain.hashtag.Hashtag;
+import elixter.blog.domain.hashtag.HashtagCount;
 import elixter.blog.domain.post.Post;
 import elixter.blog.domain.hashtag.HashtagCountInterface;
 import org.assertj.core.api.Assertions;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,17 +28,7 @@ class JpaHashtagRepositoryTest {
     }
 
     @Test
-    void searchTag() {
-        List<HashtagCountInterface> result = hashtagRepository.searchTag("소통해요");
-
-        result.forEach(res -> {
-            System.out.println("res = " + res.getTag() + " " + res.getCount());
-        });
-
-    }
-
-    @Test
-    void saveBatch() {
+    void saveAll() {
         Post testPost = Post.builder().id(193L).status(RecordStatus.exist).build();
 
         Hashtag hashtag1 = Hashtag.builder().tag("ttt").post(testPost).status(RecordStatus.exist).build();
@@ -47,5 +39,26 @@ class JpaHashtagRepositoryTest {
         ));
 
         Assertions.assertThat(hashtagList).containsAll(Arrays.asList(hashtag1, hashtag2));
+    }
+
+    @Test
+    void searchTag() {
+        Post testPost = Post.builder().id(193L).status(RecordStatus.exist).build();
+        Hashtag hashtag1 = Hashtag.builder().tag("searchTest1").post(testPost).status(RecordStatus.exist).build();
+        Hashtag hashtag2 = Hashtag.builder().tag("searchTest1").post(testPost).status(RecordStatus.exist).build();
+        Hashtag hashtag3 = Hashtag.builder().tag("searchTest1").post(testPost).status(RecordStatus.exist).build();
+        Hashtag hashtag4 = Hashtag.builder().tag("searchTest2").post(testPost).status(RecordStatus.exist).build();
+
+
+        List<Hashtag> hashtagList = (List<Hashtag>) hashtagRepository.saveAll(Arrays.asList(
+                hashtag1, hashtag2, hashtag3, hashtag4
+        ));
+
+        List<HashtagCount> result = new ArrayList<>();
+        hashtagRepository.searchTag("searchTest").forEach(res -> {
+            result.add(new HashtagCount(res.getTag(), res.getCount()));
+        });
+
+        Assertions.assertThat(result).containsAll(Arrays.asList(new HashtagCount("searchTest1", 3L), new HashtagCount("searchTest2", 1L)));
     }
 }
