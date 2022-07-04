4f6319522d8a622ed3b811bee614328cb0890a1e
elixter <ltw971@naver.com>
2022-07-04 14:20:38 +0900

    fix: postService. getAllPosts not have page infomation fixed

--- blog/src/main/java/elixter/blog/dto/post/GetAllPostsResponseDto.java
+++ blog/src/main/java/elixter/blog/dto/post/GetAllPostsResponseDto.java
@@ -1,9 +1,12 @@
 package elixter.blog.dto.post;
 
+import elixter.blog.dto.PageDto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -15,8 +18,14 @@ import java.util.List;
 @ToString
 public class GetAllPostsResponseDto implements Serializable {
     private List<GetPostResponseDto> posts;
+    private PageDto page;
 
     public GetAllPostsResponseDto() {
         posts = new ArrayList<>();
+    }
+
+    public GetAllPostsResponseDto(Pageable pageable, Long totalElem, Integer totalPage) {
+        this.posts = new ArrayList<>();
+        this.page = new PageDto(pageable, totalElem, totalPage);
     }
 }
