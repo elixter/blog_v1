3a8897d389f13bcb65c01ee3dde5c74476147a14
elixter <ltw971@naver.com>
2022-07-20 18:15:24 +0900

    feat: basic mail Service layouts

--- blog/src/main/java/elixter/blog/service/email/EmailServiceImpl.java
+++ blog/src/main/java/elixter/blog/service/email/EmailServiceImpl.java
@@ -2,9 +2,11 @@ package elixter.blog.service.email;
 
 import elixter.blog.constants.RecordStatus;
 import elixter.blog.domain.mail.Mail;
+import elixter.blog.exception.RestException;
 import elixter.blog.repository.mail.MailRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.mail.MailAuthenticationException;
 import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -31,21 +33,28 @@ public class EmailServiceImpl implements EmailService {
     }
 
     @Override
-    public void send(String sender, List<String> receivers, String title, String content) throws MessagingException {
+    public void send(String sender, List<String> receivers, String title, String content) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+        MimeMessageHelper messageHelper;
 
-        messageHelper.setFrom(sender);
-        messageHelper.setTo((String[]) receivers.toArray());
-        messageHelper.setSubject(title);
-        messageHelper.setText(content);
+        try {
+            messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            messageHelper.setFrom(sender);
+            messageHelper.setTo((String[]) receivers.toArray());
+            messageHelper.setSubject(title);
+            messageHelper.setText(content);
+        } catch (MessagingException e) {
+            throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "MessagingException");
+        }
 
         try {
             mailSender.send(mimeMessage);
         } catch (MailAuthenticationException e) {
             log.info("MailAuthenticationException", e);
+            throw new RestException(HttpStatus.UNAUTHORIZED, "Mail Authentication needed");
         } catch (MailSendException e) {
             log.info("MailSendException", e);
+            throw new RestException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to send mail");
         }
 
         log.info("mail={} sent successfully", mimeMessage);
