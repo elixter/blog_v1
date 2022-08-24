501e853c6238203075268f79509fbd0550538b5b
elixter <ltw971@naver.com>
2022-08-24 19:42:41 +0900

    refactor: appiled lambda to HashtagService

--- blog/src/main/java/elixter/blog/service/hashtag/HashtagServiceImpl.java
+++ blog/src/main/java/elixter/blog/service/hashtag/HashtagServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class HashtagServiceImpl implements HashtagService {
@@ -30,14 +31,9 @@ public class HashtagServiceImpl implements HashtagService {
 
     @Override
     public List<Long> createHashtags(List<Hashtag> hashtags) {
-        List<Long> result = new ArrayList<>();
-        hashtags = (List<Hashtag>) repository.saveAll(hashtags);
-
-        for (Hashtag hashtag : hashtags) {
-            result.add(hashtag.getId());
-        }
-
-        return result;
+        return repository.saveAll(hashtags).stream()
+                .map(Hashtag::getId)
+                .collect(Collectors.toList());
     }
 
     @Override
